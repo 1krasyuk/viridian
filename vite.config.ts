5f8c6bd4ab2d0def522017765386c0339ead9e27
evgeny krasyuk <46215218+1krasyuk@users.noreply.github.com>
2026-02-03 15:43:13 +0300

    chore/architecture design (#2)
    * chore: initialize project architecture and setup TanStack Router
    
    * chore: update deploy.yml
    
    * chore: fix components.json aliases
    
    * chore: add prettier config and format code with new rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
