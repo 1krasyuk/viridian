38f2d3182414c068078b970516ee65254680dc82
1krasyuk <evgeniyk687@gmail.com>
2026-03-07 15:11:25 +0300

    fix: fix api key query parameter naming in axios config

--- src/shared/lib/axios-config.ts
+++ src/shared/lib/axios-config.ts
@@ -4,6 +4,6 @@ export const http = axios.create({
   baseURL: 'https://api.coingecko.com/api/v3/',
   timeout: 10000,
   params: {
-    'x-cg-demo-api-key': import.meta.env.VITE_CG_API_KEY,
+    x_cg_demo_api_key: import.meta.env.VITE_CG_API_KEY,
   },
 })
