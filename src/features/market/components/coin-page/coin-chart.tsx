db13d1f66e37060ceeb5c2335ef2db36422ba499
1krasyuk <evgeniyk687@gmail.com>
2026-03-13 18:48:58 +0300

    feat: implement chart theme dynamic changing using useTheme hook

--- src/features/market/components/coin-page/coin-chart.tsx
+++ src/features/market/components/coin-page/coin-chart.tsx
@@ -1,9 +1,13 @@
+import { useTheme } from '@/shared/lib/theme-provider'
+
 export function CoinChart({ symbol }: { symbol: string | undefined }) {
+  const { theme } = useTheme()
+
   return (
     <>
       <iframe
         className='w-9/12 h-screen'
-        src={`https://s.tradingview.com/widgetembed/?symbol=BINANCE:${symbol}USDT&interval=60&theme=dark&style=3&hide_side_toolbar=false&autosize=true`}
+        src={`https://s.tradingview.com/widgetembed/?symbol=BINANCE:${symbol}USDT&interval=60&theme=${theme}&style=3&hide_side_toolbar=false&autosize=true`}
       ></iframe>
     </>
   )
