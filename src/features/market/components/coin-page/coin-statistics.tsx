eafcc4c5c77c60a00c864dcea522870dd98c1d12
1krasyuk <evgeniyk687@gmail.com>
2026-03-17 14:21:57 +0300

    feat: add volume and vol/mcap blocks

--- src/features/market/components/coin-page/coin-statistics.tsx
+++ src/features/market/components/coin-page/coin-statistics.tsx
@@ -8,8 +8,8 @@ export function CoinStatistics({ coin }: Props) {
   const priceChange = coin.market_data.price_change_percentage_24h
 
   return (
-    <div className='my-3'>
-      <div className='flex flex-col items-center gap-1 p-1 border border-'>
+    <div className='my-3 flex flex-col gap-2'>
+      <div className='flex flex-col items-center gap-1 p-1 border border-muted-foreground border-1.5 rounded-md'>
         <p className='text-muted-foreground text-sm'>Market cap</p>
         <div className='flex gap-3 items-center'>
           <p className='font-bold text-sm'>
@@ -34,6 +34,30 @@ export function CoinStatistics({ coin }: Props) {
               {priceChange == null ? '—' : priceChange >= 0 ? '▲' : '▼'}
             </span>
             {priceChange != null ? priceChange.toFixed(2) : ''}%
+          </p>
+        </div>
+      </div>
+      <div className='flex w-full gap-2'>
+        <div className='flex flex-col items-center gap-1 p-1 border border-muted-foreground border-1.5 rounded-md w-1/2'>
+          <p className='text-muted-foreground text-sm'>Volume</p>
+          <p className='font-bold text-sm'>
+            {coin.market_data.total_volume.usd.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              notation: 'compact',
+              maximumFractionDigits: 2,
+            })}
+          </p>
+        </div>
+        <div className='flex flex-col items-center gap-1 p-1 border border-muted-foreground border-1.5 rounded-md w-1/2'>
+          <p className='text-muted-foreground text-sm'>Vol/MCap (24h)</p>
+          <p className='font-bold text-sm'>
+            {(
+              (coin.market_data.total_volume.usd /
+                coin.market_data.market_cap.usd) *
+              100
+            ).toFixed(2)}
+            %
           </p>
         </div>
       </div>
