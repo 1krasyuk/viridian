906bcff42afd5a33503beed890dd4280ddae1e99
1krasyuk <evgeniyk687@gmail.com>
2026-06-14 18:49:39 +0300

    fix: fix panel min size and add --host command

--- src/features/market/components/coin-page/coin-page-sections.tsx
+++ src/features/market/components/coin-page/coin-page-sections.tsx
@@ -367,7 +367,7 @@ export function MobileCoinPageContent({
         /* ===== TERMINAL MOBILE VIEW ===== */
         <div className='flex-1 min-h-0'>
           <ResizablePanelGroup orientation='vertical' className='h-full'>
-            <ResizablePanel defaultSize='60%' minSize='30%'>
+            <ResizablePanel defaultSize='60%' minSize='40%'>
               <div className='h-full min-h-0 p-4'>
                 <CoinChart
                   coinId={coinId}
